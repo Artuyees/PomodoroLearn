ba5abc835e07644ccdcf1b4e9b736d3b41721926
Artur Kucinski <artuyees@gmail.com>
2022-01-06 23:39:08 +0100

    fixed intellisense error

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="h-screen w-screen bg-red-100">
+	<div class="grid justify-items-center items-center h-screen">
+		<slot />
+	</div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,24 +4,24 @@
 	let interval;
 	let isBreak = false;
 	let breakTime = 5;
-	let start = false;
+	let isStart = false;
 	let currentTurn = 0;
-	let finish = false;
+	let isFinish = false;
 	let turns = 2;
 	let turnLength = 1;
-	let originalTime = turnLength * 10;
+	let originalTime = turnLength * 60;
 	let timer = tweened(originalTime);
-	const onInterval = (callback, milliseconds) => {
-		interval = setInterval(callback, milliseconds);
+	const onInterval = (callback, ms) => {
+		interval = setInterval(callback, ms);
 
 		onDestroy(() => {
 			clearInterval(interval);
 		});
 	};
 	const startCount = () => {
-		start = !start;
+		isStart = !isStart;
 		currentTurn++;
-		$timer = $timer * turnLength;
+		$timer *= turnLength;
 		breakTime *= 60;
 		onInterval(() => {
 			$timer--;
@@ -38,7 +38,7 @@
 			if (currentTurn > turns) {
 				clearInterval(interval);
 				currentTurn = 0;
-				start = !start;
+				isStart = !isStart;
 				$timer = originalTime;
 				Finish();
 			}
@@ -48,9 +48,9 @@
 		}, 1000);
 	};
 	const Finish = () => {
-		finish = true;
+		isFinish = true;
 		setTimeout(() => {
-			finish = false;
+			isFinish = false;
 		}, 4000);
 	};
 	$: minutes = Math.floor($timer / 60);
@@ -60,35 +60,43 @@
 	$: currentTurnName = currentTurn > 1 ? 'turns' : 'turn';
 </script>
 
-{#if start}
-	{#if !isBreak}
-		<p>
-			remaining time: {#if minutes >= 1} {minutes} {minname} and {/if}
-			{seconds} seconds of {currentTurn}
-			{currentTurnName} out of {turns}
-			{turnName}
-		</p>
+<div class="">
+	{#if isStart}
+		{#if !isBreak}
+			<div>
+				<p>
+					remaining time: {#if minutes >= 1} {minutes} {minname} and {/if}
+					{seconds} seconds of {currentTurn}/{turns}
+					{turnName}
+				</p>
+			</div>
+		{:else}
+			<div>
+				<p>
+					remaining time: {#if minutes >= 1} {minutes} {minname} and {/if}
+					{seconds} seconds of break
+				</p>
+			</div>
+		{/if}
+		<div class="w-full bg-gray-200 h-5 mb-6">
+			<div
+				class="bg-blue-600 h-5"
+				style="width:{($timer / (isBreak ? breakTime : originalTime)) * 100}%"
+			/>
+		</div>
+	{:else if isFinish}
+		<div>
+			<p>Congrats</p>
+		</div>
 	{:else}
-		<p>
-			remaining time: {#if minutes >= 1} {minutes} {minname} and {/if}
-			{seconds} seconds of break
-		</p>
+		<div class="flex bg-red-500 flex-col">
+			<h1 class="">Welcome to Pomodoro app</h1>
+			<button on:click={startCount}>Start timer</button>
+			<div class="flex flex-row">
+				<input type="number" bind:value={turnLength} class="" />
+				<input type="number" bind:value={turns} />
+				<input type="number" bind:value={breakTime} />
+			</div>
+		</div>
 	{/if}
-
-	<progress max="1" value={$timer / (isBreak ? breakTime : originalTime)} />
-{:else if finish}
-	<p>Congrats</p>
-{:else}
-	<h1>Welcome to Pomodoro app</h1>
-	<button on:click={startCount}>Start timer</button>
-	<input type="number" bind:value={turnLength} />
-	<input type="number" bind:value={turns} />
-	<input type="number" bind:value={breakTime} />
-{/if}
-
-<style>
-	progress {
-		display: block;
-		width: 33%;
-	}
-</style>
+</div>
