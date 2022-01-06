26c1991c18b329c3fa761b764f6090081862a21d
Artur Kucinski <artuyees@gmail.com>
2022-01-06 22:33:16 +0100

    Cleaned code and added break

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,15 @@
 <script>
 	import { onDestroy } from 'svelte';
 	import { tweened } from 'svelte/motion';
-	import { writable } from 'svelte/store';
 	let interval;
+	let isBreak = false;
+	let breakTime = 5;
 	let start = false;
 	let currentTurn = 0;
 	let finish = false;
 	let turns = 2;
 	let turnLength = 1;
-	let originalTime = turnLength * 60;
+	let originalTime = turnLength * 10;
 	let timer = tweened(originalTime);
 	const onInterval = (callback, milliseconds) => {
 		interval = setInterval(callback, milliseconds);
@@ -21,26 +22,37 @@
 		start = !start;
 		currentTurn++;
 		$timer = $timer * turnLength;
+		breakTime *= 60;
 		onInterval(() => {
 			$timer--;
-			console.log(start, $timer, currentTurn, turns, turnLength, originalTime);
-			if ($timer == 0) {
+
+			if (Math.floor($timer) == 0 && !isBreak) {
+				isBreak = !isBreak;
+				$timer = breakTime;
+			}
+			if (Math.floor($timer) == 0 && isBreak) {
 				currentTurn++;
 				$timer = originalTime;
+				isBreak = !isBreak;
 			}
 			if (currentTurn > turns) {
 				clearInterval(interval);
 				currentTurn = 0;
-				$timer = originalTime;
-				finish();
 				start = !start;
+				$timer = originalTime;
+				Finish();
 			}
 			if ($timer < 0) {
 				clearInterval(interval);
 			}
 		}, 1000);
 	};
-
+	const Finish = () => {
+		finish = true;
+		setTimeout(() => {
+			finish = false;
+		}, 4000);
+	};
 	$: minutes = Math.floor($timer / 60);
 	$: seconds = Math.floor($timer - minutes * 60);
 	$: minname = minutes > 1 ? 'mins' : 'min';
@@ -49,19 +61,29 @@
 </script>
 
 {#if start}
-	<p>
-		remaining time: {#if minutes >= 1} {minutes} {minname} and {/if}
-		{seconds} seconds of {currentTurn}
-		{currentTurnName} out of {turns}
-		{turnName}
-	</p>
+	{#if !isBreak}
+		<p>
+			remaining time: {#if minutes >= 1} {minutes} {minname} and {/if}
+			{seconds} seconds of {currentTurn}
+			{currentTurnName} out of {turns}
+			{turnName}
+		</p>
+	{:else}
+		<p>
+			remaining time: {#if minutes >= 1} {minutes} {minname} and {/if}
+			{seconds} seconds of break
+		</p>
+	{/if}
 
-	<progress max="1" value={$timer / originalTime} />
+	<progress max="1" value={$timer / (isBreak ? breakTime : originalTime)} />
+{:else if finish}
+	<p>Congrats</p>
 {:else}
 	<h1>Welcome to Pomodoro app</h1>
 	<button on:click={startCount}>Start timer</button>
 	<input type="number" bind:value={turnLength} />
 	<input type="number" bind:value={turns} />
+	<input type="number" bind:value={breakTime} />
 {/if}
 
 <style>
