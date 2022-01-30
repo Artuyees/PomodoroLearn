6086c92b39608ab90ad0bb16a0d119c80137fee9
Artur Kucinski <artuyees@gmail.com>
2022-01-30 22:34:50 +0100

    mobile issue fix

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,7 +11,7 @@
 	/>
 </svelte:head>
 
-<div class="h-full w-screen min-h-screen bg-lightpurple">
+<div class="h-screen w-screen min-h-full bg-lightpurple">
 	<div class="grid justify-items-center items-center h-screen">
 		<slot />
 		<div class="mt-auto text-purple-200 w-full text-center justify-center">
